4ff17aa11ccf888bc9a5fe6874dec30af475af44
Eric Wiese <eric.wiese@classyllama.com>
2018-10-13 17:18:35 -0500

    Improve behavior of login / user info / knowledge base component interaction

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React, {Component} from 'react';
 import './App.css';
 import Knowledgebase from "./Containers/Knowledgebase";
-import Auth from "./Components/Auth/Auth";
+import LoginForm from "./Components/Auth/LoginForm";
 import {connect} from 'react-redux';
 import {subscribeResources} from './redux/actions/knowledgebase';
 import firebase from "firebase";
 import User from './redux/actions/user';
 import {withRouter} from "react-router";
+import UserInfo from "./Components/Auth/UserInfo";
 
 class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            authUser: null,
             user: null
         };
     }
@@ -22,11 +24,11 @@ class App extends Component {
         firebase.auth().onAuthStateChanged(function(user) {
             if (user) {
                 that.setState({
-                    user: user
+                    authUser: user
                 });
             } else {
                 that.setState({
-                    user: null
+                    authUser: null
                 });
             }
         });
@@ -54,19 +56,46 @@ class App extends Component {
         }
     }
 
+    loadUserDetails(uid) {
+        let that = this;
+
+        return firebase.firestore().collection('users').doc(uid).get().then(userDoc => {
+            that.setState({user: userDoc.data()});
+        });
+    }
+
     componentDidMount() {
         this.props.subscribeResources();
 
         this.wireUserAuthChange();
+
+        if(this.state.authUser !== null) {
+            this.loadUserDetails(this.state.authUser.uid);
+        }
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        if( this.state.authUser === null || this.state.authUser.uid !== nextState.authUser.uid) {
+            this.loadUserDetails(nextState.authUser.uid);
+        }
+
+        return true;
     }
 
     render() {
+        let component = <LoginForm />;
+
+        if(this.state.authUser !== null) {
+            if(this.state.user !== null && this.state.user.userPopulated === true) {
+                component = <Knowledgebase/>;
+            } else {
+                component = <UserInfo authUser={this.props.authUser} use={this.props.user} />;
+            }
+        }
+
         return (
             <div className="App">
-                <Auth user={this.state.user} />
-                {this.state.user !== null &&
-                    <Knowledgebase/>
-                }
+                {component}
             </div>
         );
     }
--- src/Components/Auth/Auth.js
+++ src/Components/Auth/Auth.js
@@ -1,50 +1,18 @@
 import React, {Component} from 'react';
 import './Auth.css';
-import firebase from "firebase";
 import LoginForm from "./LoginForm";
 import User from "./User";
 import UserInfo from "./UserInfo";
 
 export default class Auth extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            user: this.props.user
-        };
-    }
-
-    loadUserDetails(uid) {
-        let that = this;
-
-        return firebase.firestore().collection('users').doc(uid).get().then(userDoc => {
-            console.log(userDoc.data());
-            that.setState({user: userDoc.data()});
-        });
-    }
-
-    componentDidMount() {
-        if(this.props.user !== null) {
-            this.loadUserDetails(this.props.user.uid);
-        }
-    }
-
-    shouldComponentUpdate(nextProps, nextState) {
-        if(this.props.user === null || this.props.user.uid !== nextProps.user.uid) {
-            this.loadUserDetails(nextProps.user.uid);
-        }
-
-        return true;
-    }
-
     render() {
         let userComponent = <LoginForm />;
 
-        if(this.state.user !== null) {
+        if(this.props.user !== null) {
             if(this.props.user.userPopulated === true) {
-                userComponent = <User user={this.state.user} />;
+                userComponent = <User authUser={this.props.authUser} user={this.props.user} />;
             } else {
-                userComponent = <UserInfo user={this.state.user} />
+                userComponent = <UserInfo  />
             }
         }
 
