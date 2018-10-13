86234122a1903922dc72b17605c3adbe9b537a90
Sean Templeton <sean@templeton.io>
2018-10-13 11:02:34 -0500

    Remove description from list

--- src/Components/Knowledgebase/Article.js
+++ src/Components/Knowledgebase/Article.js
@@ -1,12 +1,9 @@
 import React, {Component} from 'react';
-import showdown from 'showdown';
 import './Article.css';
-
-const converter = new showdown.Converter();
 
 export default class Article extends Component {
     getLocalizedArticle() {
-        return this.props.article.i18n['es_ES'];
+        return this.props.article.i18n['en_US'];
     }
 
     render() {
@@ -16,7 +13,6 @@ export default class Article extends Component {
                     <div className="thumbnail" style={{backgroundImage: `url(${this.props.article.image})`}}/>
                 )}
                 <h2>{this.getLocalizedArticle().title}</h2>
-                <div className="content" dangerouslySetInnerHTML={{__html: converter.makeHtml(this.getLocalizedArticle().description)}}/>
             </div>
         );
     }
