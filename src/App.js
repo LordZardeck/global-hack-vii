eb5eea35002ceca86eb1192511799b1e5688732e
Sean Templeton <sean@templeton.io>
2018-10-13 17:05:46 -0500

    Added basic page styling

--- src/App.js
+++ src/App.js
@@ -1,28 +1,56 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import {createMuiTheme, MuiThemeProvider} from '@material-ui/core/styles';
+import Grid from "@material-ui/core/Grid/Grid";
+import CssBaseline from '@material-ui/core/CssBaseline';
+import AppBar from '@material-ui/core/AppBar';
+import Typography from "@material-ui/core/Typography/Typography";
+import {withStyles} from "@material-ui/core";
+
+const theme = createMuiTheme({
+    typography: {
+        useNextVariants: true,
+    },
+    palette: {
+        primary: {
+            main: '#0277BD'
+        },
+        background: {
+            default: '#F5F5F5'
+        }
+    },
+});
+
+const styles = {
+    appBar: {
+        boxShadow: 'none'
+    },
+    headingLogo: {
+        fontSize: '44px',
+        fontWeight: '700',
+        lineHeight: '52px',
+        margin: '5px auto 4px'
+    }
+};
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        const {classes} = this.props;
+
+        return (
+            <MuiThemeProvider theme={theme}>
+                <CssBaseline/>
+                <div className="App">
+                    <AppBar className={classes.appBar}>
+                        <Typography color="inherit" className={classes.headingLogo}>enabl</Typography>
+                    </AppBar>
+                    <Grid container direction="column" justify="flex-start" alignItems="center">
+
+                    </Grid>
+                </div>
+            </MuiThemeProvider>
+        );
+    }
 }
 
-export default App;
+export default withStyles(styles)(App);
